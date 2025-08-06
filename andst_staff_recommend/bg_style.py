f5b309d1de0b1c864408b3da228b01e1b8d49cb7
DON62698 <sniper9540@gmail.com>
2025-08-07 00:49:48 +0900

    Add files via upload

--- andst_staff_recommend/bg_style.py
+++ andst_staff_recommend/bg_style.py
@@ -5,7 +5,7 @@ def set_pixel_background():
     st.markdown("""
         <style>
         .stApp {
-            background-image: url('https://cdn.openai.com/chat-assets/user-uploads/file_00000000787061f9b295d47156b5177a/A_pixel_art_screenshot-style_monthly_tracking_inte.png');
+            background-image: url('https://cdn.openai.com/chat-assets/user-uploads/file_0000000027ac61faa89173c216cc3682/A_graphic_design_advertisement_poster_for_%22niko_an.png');
             background-size: cover;
             background-position: center;
             background-attachment: fixed;
